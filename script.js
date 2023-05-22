8226c92dc1c426fd42ba3df46072ae1985801650
Indwae <saintsgeits@gmail.com>
2023-05-22 15:26:27 +0300

    srip

--- script.js
+++ script.js
@@ -20,19 +20,43 @@ iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 });
 // ------------ Code Wars ----------------
-
-const bin = (value) => {
-    if(value === 7) return true;
-
-    let str = value.toString();
-    let res = 0;
-
-    for(let i = 0; str.length <= i; i++){
-        res += Number(str[i])**3;
+const bin = () => {
+    const apl = {
+        "a":	"1",
+        "b":	"2",
+        "c":	"3",
+        "d":	"4",
+        "e":	"5",
+        "f":	"6",
+        "g":	"7",
+        "h":	"8",
+        "i":	"9",
+        "j":	"10",
+        "k":    "11",
+        "l":    "12",
+        "m":	"13",
+        "n":	"14",
+        "o":	"15",
+        "p":    "16",
+        "q":	"17",
+        "r":    "18",
+        "s":	"19",
+        "t":    "20",
+        "u":	"21",
+        "v":	"22",
+        "w":	"23",
+        "x":	"24",
+        "y":	"25",
+        "z":    "26"
+        };
+        let res = [];
+    let tra = "The sunset sets at twelve o' clock.".split('');
+    for(ind in tra) { 
+        res.push(apl[(tra[ind]).toLowerCase()])
     }
-
-    if(res === value)
-      return true;
+    res.pop();
+    res = res.filter((x) => x !== undefined);
+    console.log(res.join(" "))
 }
 
-console.log(bin(153));
+console.log(bin());
